f7c3eff9e542ddd29d93f10edc59b53cea438cbb
kingslayerrq <rq2014@nyu.edu>
2024-01-31 13:43:25 -0500

    ids finished

---
+++ Budget-HC.py
@@ -0,0 +1,18 @@
+import sys
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    # read the input.txt
+    input = open('input.txt', 'r')
+    # parse cmd input arg
+    target = int(sys.argv[1])
+    budget = int(sys.argv[2])
+    flag = sys.argv[3]
+    
+
--- Budget-ID.py
+++ Budget-ID.py
@@ -1,10 +1,12 @@
 import sys
 
+
 class Node:
     def __init__(self, name: str, value: int, cost: int):
         self.name = name
         self.cost = int(cost)
         self.value = int(value)
+
 
 class ID:
     def __init__(self, target: int, budget: int, input):
@@ -13,39 +15,67 @@ class ID:
         self.nodes = self.constructNodes(input)
         self.adj_list = self.constructAdjList(self.budget, self.nodes)
 
+    def checkNode(self, n: Node, res: set[Node]) -> bool:
+        cur_cost = 0
+        for r in res:
+            cur_cost += r.cost
+        return True if cur_cost + n.cost <= self.budget else False
+
     # dfs
-    def dfsHelp(self, cur_node: Node, cur_depth: int, max_depth: int, visited: set[Node], res: set[Node]):
+    def dfsHelp(
+        self,
+        cur_node: Node,
+        cur_depth: int,
+        max_depth: int,
+        visited: set[Node],
+        res: set[Node],
+        flag: bool,
+    ):
         # break out of the recursion if exceeds depth
         if cur_depth > max_depth:
             return
-        # log out the result by depth
-        self.printResult(res)
         cur_depth += 1
+        res.add(cur_node)
+
+        # log out the result by depth
+        result_string = self.getResultString(res)
+        if flag:
+            print(result_string)
+        if self.checkResult(res):
+            if flag: print("\n")
+            print("Found solution ", result_string)
+            sys.exit(0)
+
         for n in self.adj_list[cur_node]:
-            if n not in visited:
+            # make sure the node is valid to add (not exceeding the budget), not visited, hasn't appeared in the result set
+            if n not in visited and self.checkNode(n, res) and n not in res:
                 res.add(n)
-                self.dfsHelp(n, cur_depth, max_depth, visited, res)
-                res.remove(n)                                                           # backtrack
+                self.dfsHelp(n, cur_depth, max_depth, visited, res, flag)
+                res.remove(n)  # backtrack
 
     # do dfs based on depth of a tree
-    def dfs(self, depth: int):
+    def dfs(self, depth: int, flag: bool):
         visited = set()
         for n in self.nodes:
             res = set()
             # initialize on first iteration since I didnt set a null node to start from
             visited.add(n)
-            res.add(n)
-            self.dfsHelp(n, 1, max_depth = depth, visited = visited, res = res)
-        
-    # call iterative deepening search with flag for display
-    def ids(self, depth: int, flag):
-        for i in range( 1, depth + 1):
-            print("Depth = {}".format(i))
-            self.dfs(i)
-            print("\n")
+            self.dfsHelp(n, 1, max_depth=depth, visited=visited, res=res, flag=flag)
 
-    # log out dfs result
-    def printResult(self, res: set[Node]):
+    # call iterative deepening search with flag for display
+    def ids(self, depth: int, flag: bool):
+        for i in range(1, depth + 1):
+            if flag:
+                print("Depth = {}".format(i))
+            self.dfs(i, flag)
+            if flag:
+                print("\n")
+        # no solution
+        print("No Solution")
+        sys.exit(0)
+
+    # format the result to result string
+    def getResultString(self, res: set[Node]) -> str:
         total_value = 0
         total_cost = 0
         res_string = ""
@@ -58,24 +88,39 @@ class ID:
         res_string = res_string.strip()
         res_string += "}. "
         res_string += "Value = {}. Cost = {}.".format(total_value, total_cost)
-        print(res_string)
+        return res_string
 
-    # construct nodes from input
-    def constructNodes(self, input) -> list[Node]:
+    # check result
+    def checkResult(self, res: set[Node]) -> bool:
+        total_value = 0
+        total_cost = 0
+        for r in res:
+            total_value += r.value
+            total_cost += r.cost
+        return (
+            True if total_cost <= self.budget and total_value >= self.target else False
+        )
+
+    # construct nodes from input file content
+    def constructNodes(self, input_content) -> list[Node]:
         nodes = []
-        lines = input.readlines()
+        lines = input_content.readlines()
         # instatiate nodes
         for line in lines:
             split_line = line.split()
+            # log an err if the input file is malformatted and exit the app
             if len(split_line) != 3:
-                return "Input file formatted incorrectly!!!"
+                print("Input file formatted incorrectly!!!", file=sys.stderr)
+                sys.exit(0)
             nodes.append(Node(split_line[0], split_line[1], split_line[2]))
-        #for n in nodes:
-            #print("Node {}, Cost {}, Value {}".format(n.name, n.cost, n.value))
+        # for n in nodes:
+        # print("Node {}, Cost {}, Value {}".format(n.name, n.cost, n.value))
         return nodes
 
     # construct Adjacency list
-    def constructAdjList(self, budget: int, nodes: list[Node]) -> dict[Node, list[Node]]:
+    def constructAdjList(
+        self, budget: int, nodes: list[Node]
+    ) -> dict[Node, list[Node]]:
         adj_list = {}
         node_num = len(nodes)
         for i in range(node_num):
@@ -87,23 +132,28 @@ class ID:
             adj_list[nodes[i]] = l
             # print("Node {} has ".format(nodes[i].name), end="")
             # for n in l:
-                # print("{}".format(n.name), end=" ")
+            # print("{}".format(n.name), end=" ")
             # print("\n")
         return adj_list
 
 
-
-
-
-
-
 if __name__ == "__main__":
     # read the input.txt
-    input = open('input.txt', 'r')
+    input = open("input.txt", "r")
+
     # parse cmd input arg
+    if len(sys.argv) != 4:
+        print("Invalid arg count!!", file=sys.stderr)
+        sys.exit(0)
     target = int(sys.argv[1])
     budget = int(sys.argv[2])
     flag = sys.argv[3]
+    if flag == "V":
+        flag = True
+    elif flag == "C":
+        flag = False
+    else:
+        print("Invalid flag input!!", file=sys.stderr)
+        sys.exit(0)
     ID_instance = ID(target, budget, input)
-    ID_instance.ids(2, flag)
-
+    ID_instance.ids(3, flag)
